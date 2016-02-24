94296d1e6146fa556f6a79d87bb6e1f13e19b8d4
srenaudon <srenaudon@nullepart.org>
2016-02-24 23:18:16 +0100

    KPI taxi

---
+++ src/js/kpi-active-taxi.js
@@ -0,0 +1,72 @@
+/* KPI Active taxis  */
+(function () {
+    $(function () {
+
+        var kpiAnchor = 'wa-kpi-active-taxi';
+
+        function createKPI(anchor) {
+
+            $("#" + anchor).kendoChart({
+
+                legend: {
+                    position: "bottom",
+                    labels: {
+                        color: waEC3.config.style.color
+                    }
+
+                },
+                chartArea: {
+                    background: ""
+                },
+                seriesDefaults: {
+
+                    style: "smooth"
+                },
+                series: [{
+                    type: "line",
+                    name: "actual taxi trips",
+                    data: [700, 800, 700, 750, 900, 1000, 1700, 2100, 2000, 1900, 1800, 1319, 1450, 1800, 1900, 2000]//, 2300, 2000, 1900, 1300, 1100, 800, 700, 500]
+                },
+                    {
+                        type: "column",
+                        opacity: 0.3,
+                        name: "average taxi trips",
+                        data: [500, 500, 600, 600, 800, 1000, 2000, 2200, 2200, 1700, 1300, 1000, 1000, 1500, 2000, 2000, 2500, 2500, 1800, 1800, 900, 900, 500, 500]
+                    }],
+                valueAxis: {
+                    labels: {
+                        format: "{0}",
+                        color: waEC3.config.style.color
+                    },
+                    line: {
+                        visible: false
+                    },
+                    axisCrossingValue: 0
+                },
+                categoryAxis: {
+                    categories: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
+                    majorGridLines: {
+                        visible: false
+                    },
+                    labels: {
+                        rotation: "auto",
+                        color: waEC3.config.style.color
+                    }
+                },
+                tooltip: {
+                    visible: true,
+                    format: "{0}",
+                    template: "#= series.name #: #= value #"
+                }
+
+            });
+        }
+
+        createKPI(kpiAnchor);
+
+    });
+
+
+})();
+
+
--- src/js/kpi-alert-event-backlog.js
+++ src/js/kpi-alert-event-backlog.js
@@ -1,20 +1,15 @@
-/* Operators load factor KPI */
-
+/* KPI Backlog  */
 (function () {
     $(function () {
 
-
-       // var dataSupervisor = new kendo.data.ObservableArray(dataScenarioSupervisor[0]);
-      //  var dataCaller = new kendo.data.ObservableArray(dataScenarioCaller[0]);
-      //  var dataLiaison = new kendo.data.ObservableArray(dataScenarioLiaison[0]);
         var kpiAnchor = 'wa-kpi-alert-event-backlog';
 
         function createKPI(anchor) {
-            /* Backlog  */
-            $("#"+anchor).kendoChart({
+
+            $("#" + anchor).kendoChart({
 
                 legend: {
-                    visible: true
+                    visible: false
                 },
                 seriesDefaults: {
                     type: "column",
@@ -77,16 +72,15 @@
                     line: {
                         visible: false
                     },
-                    notes:{
-                        position:"bottom",
-                        label:{
+                    notes: {
+                        position: "bottom",
+                        label: {
                             template: "#= series.stack.group #",
-                            visible:true,
-                            format:'{0}',
-                            position:'outside'
+                            visible: true,
+                            format: '{0}',
+                            position: 'outside'
                         }
                     }
-
                 },
                 tooltip: {
                     visible: true,
@@ -94,181 +88,14 @@
                 },
                 chartArea: {
                     background: ''
-                },
-                legend: {
-                    visible: false
-
                 }
             });
-
-
-        }
-
-        function animateKPI(anchor, indata, inscenario) {
-
-            var data = indata;
-            var scenario = inscenario;
-            var intervalID;
-
-            var clos = function (data, scenario) {
-                var count = 0;
-                setInterval(function () {
-                    count++;
-                    if (count > 2) {
-                        count = 0;
-                    }
-                    data.splice(0, 2);
-                    data.push(scenario[count][0]);
-                    data.push(scenario[count][1]);
-
-                    if (count === 1) {
-                        intervalID = alertKPI('wa-kpi-operators-load-factor', true);
-                    } else {
-                        alertKPI('wa-kpi-operators-load-factor', false, intervalID);
-                    }
-
-                }, (waEC3.config.kpi.animationStep * 60));//change every min
-
-            }
-            clos(data, scenario);
         }
 
         createKPI(kpiAnchor);
 
-
-      //  animateKPI("wa-kpi-supervisor-load-factor", dataSupervisor, dataScenarioSupervisor);
-      //  animateKPI("wa-kpi-liaison-officer-load-factor", dataLiaison, dataScenarioLiaison);
-      //  animateKPI("wa-kpi-call-taker-load-factor", dataCaller, dataScenarioCaller);
-
     });
 
-
-    /* data for demo scenario */
-
-    var dataScenarioSupervisor = [
-        [{
-            "source": "free",
-            "percentage": 30,
-            "explode": true,
-            color: waEC3.config.style.colorOKAlt
-
-        },
-            {
-                "source": "occupied",
-                "percentage": 70,
-                color: waEC3.config.style.colorWatch
-            }
-        ],
-        [{
-            "source": "free",
-            "percentage": 20,
-            "explode": true,
-            color: waEC3.config.style.colorOKAlt
-
-        },
-            {
-                "source": "occupied",
-                "percentage": 80,
-                color: waEC3.config.style.colorNOK
-            }],
-        [{
-            "source": "free",
-            "percentage": 50,
-            "explode": true,
-            color: waEC3.config.style.colorOKAlt
-
-        },
-            {
-                "source": "occupied",
-                "percentage": 50,
-                color: waEC3.config.style.colorOK
-            }
-        ]
-
-    ];
-    var dataScenarioLiaison = [
-        [{
-            "source": "free",
-            "percentage": 40,
-            "explode": true,
-            color: waEC3.config.style.colorOKAlt
-
-        },
-            {
-                "source": "occupied",
-                "percentage": 60,
-                color: waEC3.config.style.colorOK
-            }
-        ],
-        [{
-            "source": "free",
-            "percentage": 10,
-            "explode": true,
-            color: waEC3.config.style.colorOKAlt
-
-        },
-            {
-                "source": "occupied",
-                "percentage": 90,
-                color: waEC3.config.style.colorNOK
-            }],
-        [{
-            "source": "free",
-            "percentage": 70,
-            "explode": true,
-            color: waEC3.config.style.colorOKAlt
-
-        },
-            {
-                "source": "occupied",
-                "percentage": 30,
-                color: waEC3.config.style.colorOK
-            }
-        ]
-
-    ];
-
-    var dataScenarioCaller = [
-        [{
-            "source": "free",
-            "percentage": 40,
-            "explode": true,
-            color: waEC3.config.style.colorOKAlt
-
-        },
-            {
-                "source": "occupied",
-                "percentage": 60,
-                color: waEC3.config.style.colorOK
-            }
-        ],
-        [{
-            "source": "free",
-            "percentage": 10,
-            "explode": true,
-            color: waEC3.config.style.colorOKAlt
-
-        },
-            {
-                "source": "occupied",
-                "percentage": 90,
-                color: waEC3.config.style.colorNOK
-            }],
-        [{
-            "source": "free",
-            "percentage": 80,
-            "explode": true,
-            color: waEC3.config.style.colorOKAlt
-
-        },
-            {
-                "source": "occupied",
-                "percentage": 20,
-                color: waEC3.config.style.colorOK
-            }
-        ]
-
-    ];
 
 })();
 
